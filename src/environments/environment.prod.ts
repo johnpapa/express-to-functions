d8f65b9421c0286029e8773982992971cb63438f
John Papa <john@johnpapa.net>
2020-03-16 18:58:19 -0400

    fixing linting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // API: 'api'
-  API: 'https://papa-angularconnect2019.azurewebsites.net/api'
-  // API: 'https://papa-heroes-node-api.azurewebsites.net/api'
+  API: 'api'
+  // API: 'https://papa-angularconnect2019.azurewebsites.net/api'
 };
